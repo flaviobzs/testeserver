58721de7379695e35e21be6faad3c003fdbc99a2
flaviobzs <bzsflavio@gmail.com>
2019-07-28 21:53:12 -0300

    modulo testes finalizado

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -3,7 +3,9 @@ import bcrypt from 'bcryptjs';
 import app from '../../src/app';
 
 import truncate from '../util/truncate';
-import User from '../../src/app/models/User';
+// import User from '../../src/app/models/User';
+// ao inves de importar o usuario, será importado o gerador de dados
+import factory from '../factories';
 
 describe('User', () => {
   beforeEach(async () => {
@@ -11,9 +13,14 @@ describe('User', () => {
   });
 
   it('should encrypt user password when new user created', async () => {
-    const user = await User.create({
-      name: 'FlavioBS',
-      email: 'flaviobs_@hotmail.com',
+    // const user = await User.create({
+    //   name: 'FlavioBS',
+    //   email: 'flaviobs_@hotmail.com',
+    //   password: '123456',
+    // });
+
+    const user = await factory.create('User', {
+      // permanecer com a senha pois aqui está sendo comparadas
       password: '123456',
     });
 
@@ -23,32 +30,40 @@ describe('User', () => {
   });
 
   it('should be able to register', async () => {
+    // criar objeto com os atributos
+    const user = await factory.attrs('User');
+
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'FlavioBS',
-        email: 'flaviobs_@hotmail.com',
-        password: '123456',
-      });
+      .send(user);
+    // .send({
+    //   name: 'FlavioBS',
+    //   email: 'flaviobs_@hotmail.com',
+    //   password: '123456',
+    // });
     expect(response.body).toHaveProperty('id');
   });
 
   it('should not be able to register with duplicated email', async () => {
+    const user = await factory.attrs('User');
+
     await request(app)
       .post('/users')
-      .send({
-        name: 'FlavioBS',
-        email: 'flaviobs_@hotmail.com',
-        password: '123456',
-      });
+      .send(user);
+    // .send({
+    //   name: 'FlavioBS',
+    //   email: 'flaviobs_@hotmail.com',
+    //   password: '123456',
+    // });
 
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'FlavioBS',
-        email: 'flaviobs_@hotmail.com',
-        password: '123456',
-      });
+      .send(user);
+    // .send({
+    //   name: 'FlavioBS',
+    //   email: 'flaviobs_@hotmail.com',
+    //   password: '123456',
+    // });
 
     expect(response.status).toBe(400);
   });
